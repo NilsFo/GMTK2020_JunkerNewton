930c34ed8c0e5bcf9f8b2d37b0e5371c9e83d8a0
S-Embers <stephan.embers@gmail.com>
2020-07-12 12:09:11 +0200

    Added Cat to level 5 as Roadblog

--- game.py
+++ game.py
@@ -761,7 +761,7 @@ class Level4(BaseLevel):
         self.astronaut.angle = 0.15
         self.astronaut_state["has_sat"] = False
 
-        self.next_level = Level3
+        self.next_level = Level5
 
         self.satellite, c = create_satellite_body(self.worldgroup, position=(35*32,15*32))
         self.physspace.add(self.satellite, c)
@@ -778,6 +778,56 @@ class Level4(BaseLevel):
         self.physspace.add(create_clutter_body(self.worldgroup, "platine", position=(15.8*32,9.8*32), velocity=(0,0), angular_velocity=0))
         self.physspace.add(create_clutter_body(self.worldgroup, "cat", position=(30.5*32,21.5*32), velocity=(-0.7, 0.1), angular_velocity=0.0))
         self.win_trigger = pymunk.BB(10*32,12*32,16*32,18*32)
+
+        def collect(arbiter, space, data):
+            collectible = arbiter.shapes[1]
+            space.remove(collectible, collectible.body)
+            associated_sprites = filter(lambda s: collectible in s.physbody.shapes, self.worldgroup.sprites())
+            self.worldgroup.remove(*associated_sprites)
+            self.astronaut_state["has_sat"] = True
+            print(self.astronaut_state)
+
+            self.astronaut_sprite_normal = img_astronaut_sat
+            self.astronaut_sprite_tf = img_astronaut_tf_sat
+            self.astronaut_sprite_tr = img_astronaut_tr_sat
+            self.astronaut_sprite_tb = img_astronaut_tb_sat
+            self.astronaut_sprite_tl = img_astronaut_tl_sat
+            return False
+
+        handler = self.physspace.add_collision_handler(collision_types["astronaut"], collision_types["collectible"])
+        handler.pre_solve = collect
+
+    def check_win_condition(self):
+        return super().check_win_condition() and self.astronaut_state["has_sat"]
+
+    def get_signal_position(self):
+        return self.satellite.position if not self.astronaut_state["has_sat"] else None
+
+class Level5(BaseLevel):
+    def __init__(self, game):
+        super().__init__(game, map_name="level5.tmx")
+
+        self.astronaut.position = (23*32, 15*32)
+        self.astronaut.angle = 0.15
+        self.astronaut_state["has_sat"] = False
+
+        self.next_level = None
+
+        self.satellite, c = create_satellite_body(self.worldgroup, position=(9*32,13.5*32))
+        self.physspace.add(self.satellite, c)
+        self.physspace.add(create_asteroid_body(self.worldgroup, position=(5*32,26*32), velocity=(20,0)))
+        self.physspace.add(create_asteroid_body(self.worldgroup, position=(15*32,26*32), velocity=(0,0)))
+        self.physspace.add(create_asteroid_body(self.worldgroup, position=(24*32,26*32), velocity=(0,0)))
+
+        self.physspace.add(create_clutter_body(self.worldgroup, "beer", position=(30*32,11*32), velocity=(.3,.1), rotation=-0.7, angular_velocity=0.7))
+        self.physspace.add(create_clutter_body(self.worldgroup, "beer", position=(26*32,6*32), velocity=(.1,-.1), rotation=2.5, angular_velocity=-0.2))
+        self.physspace.add(create_clutter_body(self.worldgroup, "beer", position=(20*32,5*32), velocity=(.05,.1), rotation=2.3, angular_velocity=-0.1))
+        self.physspace.add(create_clutter_body(self.worldgroup, "beer", position=(16.1*32,18.*32), velocity=(0,-.1), rotation=0.5, angular_velocity=-0.1))
+        self.physspace.add(create_clutter_body(self.worldgroup, "wrench", position=(30*32,18*32), velocity=(-10,-5)))
+        self.physspace.add(create_clutter_body(self.worldgroup, "can", position=(24*32,23*32)))
+        self.physspace.add(create_clutter_body(self.worldgroup, "platine", position=(3*32,13*32), velocity=(0,0), angular_velocity=0))
+        self.physspace.add(create_clutter_body(self.worldgroup, "cat", position=(17*32,5*32), velocity=(0, 0), angular_velocity=2))
+        self.win_trigger = pymunk.BB(20*32,12*32,26*32,18*32)
 
         def collect(arbiter, space, data):
             collectible = arbiter.shapes[1]
@@ -1279,7 +1329,7 @@ def main():
     game.setup()
     print("Finished loading")
 
-    game.current_screen = Level4(game)
+    game.current_screen = Level5(game)
     game.current_screen.on_screen_enter()
 
     print("Starting Game Loop")
